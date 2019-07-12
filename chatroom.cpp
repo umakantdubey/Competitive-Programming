31958dad15d290b2c3550cf08e0e9eedbe75ef89
Umakant Dubey <happydubey09@gmail.com>
2019-07-12 08:55:15 +0530

    Codeforces solution

--- chatroom.cpp
+++ chatroom.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 int main()
 {
+string s;
+cin>>s;
+string p="hello";
+int l= s.length();
+int j=0;
+int k=0;
+for(int i=0;i<l;i++)
+{
 
-    cout<<"hello world"<<endl;
+    if(s[i]==p[j])
+    {
+        k++;
+        j++;
+
+    }
 }
+if(k==5)
+{
+
+    cout<<"YES"<<endl;
+}
+else
+{
+
+    cout<<"NO"<<endl;
+}
+
+}
+
